9b762137bfed8d94fbd01b13a8b1ece9f159f22b
mairh <ujjwal.mairh@gmail.com>
2024-09-12 17:03:53 +0300

    update smart home

--- src/sections/_home/view/smart-home-view.tsx
+++ src/sections/_home/view/smart-home-view.tsx
@@ -2,11 +2,12 @@
 
 import { _testimonials } from 'src/_mock';
 
-import HomeTestimonial from '../testimonial/home-testimonial';
 import SmartHomePricing from '../smart-home/smart-home-pricing';
 import SmartHomeInclude from '../smart-home/smart-home-include';
-import SmartHomeBenefits from '../smart-home/smart-home-benefits';
+import SmartHomeHighlights from '../smart-home/smart-home-highlights';
 import SmartHomeHowItWork from '../smart-home/smart-home-how-it-work';
+import SmartHomeBenefits from '../smart-home/smart-home-benefits';
+import HomeTestimonial from '../testimonial/home-testimonial';
 
 // ----------------------------------------------------------------------
 
@@ -16,6 +17,8 @@ export default function SmartHomeView() {
       <SmartHomePricing />
 
       <SmartHomeInclude />
+
+      <SmartHomeHighlights />
 
       <SmartHomeHowItWork />
 
