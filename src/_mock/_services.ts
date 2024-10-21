7b42b96e377c01f95ff534609d049bbf68d506a6
mairh <ujjwal.mairh@gmail.com>
2024-10-21 20:42:06 +0300

    Add new stripe links

--- src/_mock/_services.ts
+++ src/_mock/_services.ts
@@ -37,37 +37,6 @@ export const HOME_SERVICE_OPTIONS = [
   { value: 'Etsitkö muuta palvelua?', label: 'Etsitkö muuta palvelua?' },
 ];
 
-export const HOME_SERVICE_PAYMENT_LINKS = [
-  { value: 'https://book.stripe.com/fZeaIieNVeMd9q09G2', label: 'Ilmastointityöt 69€/h' },
-  { value: 'https://book.stripe.com/9AQbMmgW36fHdGgaK7', label: 'Asbestikartoitus 199€/h' },
-  { value: 'https://book.stripe.com/00geYy5dleMd45G3hG', label: 'Kotisiivous 44€/h' },
-  { value: 'https://book.stripe.com/8wM4jU49h8nPdGg5pP', label: 'Muuttosiivous 44€/h' },
-  { value: 'https://book.stripe.com/3cs17I6hp47z8lWg4u', label: 'Toimistosiivous 44€/h' },
-  { value: 'https://book.stripe.com/7sIbMm49h0Vn59Kf0r', label: 'Remonttisiivous 44€/h' },
-  { value: 'https://book.stripe.com/7sI5nY7lt33vdGgf0s', label: 'Ikkunanpesu 44€/h' },
-  { value: 'https://book.stripe.com/6oEg2C9tB9rTby86tX', label: 'Kuljetuspalvelut 59€/h' },
-  { value: 'https://book.stripe.com/7sI8Aa8px9rT7hScSm', label: 'Kotiapu 39€/h' },
-  { value: 'https://book.stripe.com/8wM7w65dl0Vn9q09Gb', label: 'Sähköasennukset 69€/h' },
-  { value: 'https://book.stripe.com/5kA5nY35d1Zr7hS6u2', label: 'Elektroniikkahuolto 69€/h' },
-  { value: 'https://book.stripe.com/28obMm8px47z31C6u0', label: 'Asennuspalvelut 54€/h' },
-  { value: 'https://book.stripe.com/bIY7w67lt6fH1Xyg4B', label: 'Kodinkonehuolto 69€/h' },
-  { value: 'https://book.stripe.com/9AQ17IeNV6fH1Xy5pZ', label: 'Kantoapu 44€/h' },
-  { value: 'https://book.stripe.com/aEU3fQaxF1Zr6dOf0A', label: 'Muutto 59€/h' },
-  { value: 'https://book.stripe.com/14k3fQ0X5bA17hS05H', label: 'Putkityöt 69€/h' },
-  {
-    value: 'https://book.stripe.com/dR6dUu6hp0VneKk05I',
-    label: 'Kiinteistöhuolto ja isännöintipalvelut 44€/h',
-  },
-  { value: 'https://book.stripe.com/4gweYybBJcE559K4lZ', label: 'Kylmälaitteiden asennus 74€/h' },
-  { value: 'https://book.stripe.com/28o2bM9tB5bD8lW8Cg', label: 'Remontit ja rakentaminen 56€/h' },
-  {
-    value: 'https://book.stripe.com/4gwaIifRZ33vdGgbOt',
-    label: 'Pihan muutos- ja rakennustyöt 56€/h',
-  },
-  { value: 'https://book.stripe.com/28odUu5dl47z59K9Gm', label: 'Pihatyöt 56€/h' },
-  { value: 'https://www.hero24.com/koti-palvelu', label: 'Etsitkö muuta palvelua?' },
-];
-
 export const _services = [...Array(22)].map((_, index) => {
   const location = countries.map((option) => option.label)[index + 1];
 
@@ -125,65 +94,65 @@ export const _services = [...Array(22)].map((_, index) => {
     stripePaymentLink2: [...Array(1)].map((__, itemIndex) =>
       _mock.stripePaymentLink(itemIndex + 1)
     ),
-    stripePaymentLink3: [...Array(1)].map((__, itemIndex) =>
+    stripePaymentLink3: [...Array(5)].map((__, itemIndex) =>
       _mock.stripePaymentLink(itemIndex + 2)
     ),
-    stripePaymentLink4: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 3)
-    ),
-    stripePaymentLink5: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 4)
-    ),
-    stripePaymentLink6: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 5)
-    ),
-    stripePaymentLink7: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 6)
-    ),
-    stripePaymentLink8: [...Array(1)].map((__, itemIndex) =>
+    stripePaymentLink4: [...Array(5)].map((__, itemIndex) =>
       _mock.stripePaymentLink(itemIndex + 7)
     ),
-    stripePaymentLink9: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 8)
-    ),
-    stripePaymentLink10: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 9)
-    ),
-    stripePaymentLink11: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 10)
-    ),
-    stripePaymentLink12: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 11)
-    ),
-    stripePaymentLink13: [...Array(1)].map((__, itemIndex) =>
+    stripePaymentLink5: [...Array(5)].map((__, itemIndex) =>
       _mock.stripePaymentLink(itemIndex + 12)
     ),
-    stripePaymentLink14: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 13)
-    ),
-    stripePaymentLink15: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 14)
-    ),
-    stripePaymentLink16: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 15)
-    ),
-    stripePaymentLink17: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 16)
-    ),
-    stripePaymentLink18: [...Array(1)].map((__, itemIndex) =>
+    stripePaymentLink6: [...Array(5)].map((__, itemIndex) =>
       _mock.stripePaymentLink(itemIndex + 17)
     ),
+    stripePaymentLink7: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 22)
+    ),
+    stripePaymentLink8: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 23)
+    ),
+    stripePaymentLink9: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 24)
+    ),
+    stripePaymentLink10: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 25)
+    ),
+    stripePaymentLink11: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 26)
+    ),
+    stripePaymentLink12: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 27)
+    ),
+    stripePaymentLink13: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 28)
+    ),
+    stripePaymentLink14: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 29)
+    ),
+    stripePaymentLink15: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 30)
+    ),
+    stripePaymentLink16: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 31)
+    ),
+    stripePaymentLink17: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 32)
+    ),
+    stripePaymentLink18: [...Array(1)].map((__, itemIndex) =>
+      _mock.stripePaymentLink(itemIndex + 33)
+    ),
     stripePaymentLink19: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 18)
+      _mock.stripePaymentLink(itemIndex + 34)
     ),
     stripePaymentLink20: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 19)
+      _mock.stripePaymentLink(itemIndex + 35)
     ),
     stripePaymentLink21: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 20)
+      _mock.stripePaymentLink(itemIndex + 36)
     ),
     stripePaymentLink22: [...Array(1)].map((__, itemIndex) =>
-      _mock.stripePaymentLink(itemIndex + 21)
+      _mock.stripePaymentLink(itemIndex + 37)
     ),
     highlights1: _mock.serviceSentences1(index),
     highlights2: _mock.serviceSentences2(index),
